6ace7e60e723e0b5243fab455ac592bb850dea81
Amedamin <ahmedqryqr@gmail.com>
2024-04-04 14:50:34 -0700

    done

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,43 +7,21 @@ from datetime import datetime
 import os
 
 env.hosts = ["100.25.16.27", "54.237.109.157"]
-env.user = 'ubuntu'
-env.key_filename = ['my_ssh_private_key']
 
 
 def do_clean(number=0):
-    """
-    Deletes out-of-date archives
-    """
-    try:
-        number = int(number)
-    except ValueError:
-        number = 1
-
-    if number < 1:
-        number = 1
-
-    # Local path to the archives
-    local_path = "./versions"
-
-    with cd('/data/web_static/releases'):
-        # Get list of archives
-        archives = run("ls -t").split()
-
-        # Keep only the most recent 'number' archives
-        to_delete = archives[number:]
-
-        for archive in to_delete:
-            if archive.strip():
-                run("rm -f {}".format(archive))
-
-    with lcd(local_path):
-        # Get list of local archives
-        local_archives = local("ls -t", capture=True).split()
-
-        # Keep only the most recent 'number' local archives
-        to_delete_local = local_archives[number:]
-
-        for local_archive in to_delete_local:
-            if local_archive.strip():
-                local("rm -f {}".format(local_archive))
+    """Deletes out-of-date archives"""
+    files = local("ls -1t versions", capture=True)
+    file_names = files.split("\n")
+    n = int(number)
+    if n in (0, 1):
+        n = 1
+    for i in file_names[n:]:
+        local("rm versions/{}".format(i))
+    dir_server = run("ls -1t /data/web_static/releases")
+    dir_server_names = dir_server.split("\n")
+    for i in dir_server_names[n:]:
+        if i == 'test':
+            continue
+        run("rm -rf /data/web_static/releases/{}"
+            .format(i))
