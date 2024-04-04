d9ed6df72fdae8c0def2b99bcdbc1df0e18ea4bb
Amedamin <ahmedqryqr@gmail.com>
2024-04-04 15:01:51 -0700

    done

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Fabric script that deletes out-of-date archives
-"""
+"""Fabric script"""
 from fabric.api import *
 from datetime import datetime
 import os
@@ -10,18 +8,20 @@ env.hosts = ["100.25.16.27", "54.237.109.157"]
 
 
 def do_clean(number=0):
-    """Deletes out-of-date archives"""
-    files = local("ls -1t versions", capture=True)
-    file_names = files.split("\n")
-    n = int(number)
-    if n in (0, 1):
-        n = 1
-    for i in file_names[n:]:
-        local("rm versions/{}".format(i))
-    dir_server = run("ls -1t /data/web_static/releases")
-    dir_server_names = dir_server.split("\n")
-    for i in dir_server_names[n:]:
-        if i == 'test':
-            continue
-        run("rm -rf /data/web_static/releases/{}"
-            .format(i))
+    """Deletes"""
+    with cd("/data/web_static/releases"):
+        files = run("ls -1t").split("\n")
+        n = int(number)
+        if n in (0, 1):
+            n = 1
+        for i in files[n:]:
+            if i != "test":
+                run("rm -rf {}".format(i))
+    
+    with lcd("versions"):
+        files = local("ls -1t", capture=True).split("\n")
+        n = int(number)
+        if n in (0, 1):
+            n = 1
+        for i in files[n:]:
+            local("rm {}".format(i))
